4d4d0af6ff4e3d07d08e81ae15e2cc5c09938c0f
parastoo naderi <roniaania@gmail.com>
2024-10-19 18:11:05 +0330

    i have created the Entry and label and Button elements

--- main.py
+++ main.py
@@ -7,10 +7,52 @@ db=Client['CRUD']
 persons=db['persons1']
 
 win=Tk()
-win.geometry("850x600")
-# win.attributes('-fullscreen',True)
+# win.geometry("850x600")
+win.attributes('-fullscreen',True)
 win.title('CRUD')
 win.iconbitmap('icons/python_18894.ico')
 win.configure(background='#914d4d')
+
+#TXTbox
+txtName=Entry(win,width=15,bd=5,font=('arial',15,'bold'),bg='#a18282',fg='white',textvariable=Name,justify='center')
+txtName.bind('<KeyRelease>',ActiveBtn)
+txtName.place(x=100,y=100)
+
+txtFamily=Entry(win,width=15,bd=5,font=('arial',15,'bold'),bg='#a18282',fg='white',textvariable=Family,justify='center')
+txtFamily.bind('<KeyRelease>',ActiveBtn)
+txtFamily.place(x=100,y=160)
+
+txtField=Entry(win,width=15,bd=5,font=('arial',15,'bold'),bg='#a18282',fg='white',textvariable=Field,justify='center')
+txtField.bind('<KeyRelease>',ActiveBtn)
+txtField.place(x=100,y=220)
+
+
+txtAge=Entry(win,width=15,bd=5,font=('arial',15,'bold'),bg='#a18282',fg='white',textvariable=Age,justify='center')
+txtAge.bind('<KeyRelease>',ActiveBtn)
+txtAge.place(x=100,y=280)
+
+#LBL
+lblName=Label(win,text='Name',font=('arial',12,'bold'),bg='#a18282',fg='white')
+lblName.place(x=20,y=100)
+
+lblFamily=Label(win,text='Family',font=('arial',12,'bold'),bg='#a18282',fg='white')
+lblFamily.place(x=20,y=160)
+
+lblField=Label(win,text='Filed',font=('arial',12,'bold'),bg='#a18282',fg='white')
+lblField.place(x=20,y=220)
+
+lblAge=Label(win,text='Age',font=('arial',12,'bold'),bg='#a18282',fg='white')
+lblAge.place(x=20,y=280)
+
+#BTN
+
+btnRegister=Button(win,text='register',width=10,font=('arial',12,'bold'),bg='#a18282',fg='white')
+btnRegister.configure(state=DISABLED)
+btnRegister.bind('<Enter>',changeButtonStyleWithHoverRegister)
+btnRegister.bind('<Leave>',changeButtonStyleWithHoverToSelfRegister)
+btnRegister.bind('<Button-1>',OnClickRegister)
+btnRegister.place(x=125,y=350)
+
+
 
 win.mainloop()
